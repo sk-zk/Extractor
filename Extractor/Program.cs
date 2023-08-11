a720425a4cb5ee349f0ae642b63bc7bb8e318a03
sk-zk <sk-zk@users.noreply.github.com>
2023-08-12 00:33:05 +0200

    Add --raw flag

--- Extractor/Program.cs
+++ Extractor/Program.cs
@@ -18,13 +18,14 @@ namespace Extractor
             bool all = false;
             string start = "/";
             bool help = false;
+            bool raw = false;
             var p = new OptionSet()
             {
                 { "<>",
                     "Path",
                     x => { path = x; } },
                 { "a|all",
-                    "Extracts every .scs file in the directory.",
+                    "Extracts every .scs archive in the directory.",
                     x => { all = true; } },
                 { "d=",
                     $"The output directory.\nDefault: {destination}",
@@ -32,6 +33,12 @@ namespace Extractor
                 { "p=",
                     $"Partial extraction, e.g. \"-p=/map\".",
                     x => { start = x; } },
+                { "r|raw",
+                    "Directly dumps the contained files with their hashed filenames rather than " +
+                    "traversing the archive's directory tree." + 
+                    "This allows for the extraction of base_cfg.scs, core.scs " +
+                    "and\nlocale.scs, which do not include a top level directory listing.",
+                    x => { raw = true; } },
                 { "?|h|help",
                     $"Prints this message.",
                     x => { help = true; } },
@@ -47,7 +54,7 @@ namespace Extractor
 
             if (all)
             {
-                ExtractAllInDirectory(path, start);
+                ExtractAllInDirectory(path, start, raw);
             }
             else
             {
@@ -56,31 +63,45 @@ namespace Extractor
                     Console.Error.WriteLine($"\"{path}\" is not a file or does not exist.");
                     Environment.Exit(-1);
                 }
-                ExtractScsFile(path, start);
+                if (raw)
+                {
+                    ExtractScsWithoutTopLevel(path);
+                }
+                else
+                {
+                    ExtractScs(path, start);
+                }
             }
         }
 
-        private static void ExtractAllInDirectory(string path, string start)
+        private static void ExtractAllInDirectory(string directory, string start, bool raw = false)
         {
-            if (!Directory.Exists(path))
+            if (!Directory.Exists(directory))
             {
-                Console.Error.WriteLine($"\"{path}\" is not a directory or does not exist.");
+                Console.Error.WriteLine($"\"{directory}\" is not a directory or does not exist.");
                 Environment.Exit(-1);
             }
-            foreach (var scsPath in Directory.EnumerateFiles(path, "*.scs"))
+            foreach (var scsPath in Directory.EnumerateFiles(directory, "*.scs"))
             {
-                ExtractScsFile(scsPath, start);
+                if (raw)
+                {
+                    ExtractScsWithoutTopLevel(directory);
+                }
+                else
+                {
+                    ExtractScs(scsPath, start);
+                }
             }
         }
 
-        private static void ExtractScsFile(string scsPath, string start)
+        private static void ExtractScs(string scsPath, string start)
         {
             var reader = HashFsReader.Open(scsPath);
 
             switch (reader.EntryExists(start))
             {
                 case EntryType.Directory:
-                    Extract(reader, start, Path.GetFileName(scsPath));
+                    ExtractDirectory(reader, start, Path.GetFileName(scsPath));
                     break;
                 case EntryType.File:
                     ExtractSingleFile(start, reader);
@@ -90,7 +111,28 @@ namespace Extractor
             }
         }
 
-        private static void Extract(HashFsReader r, string directory, string scsName)
+        private static void ExtractScsWithoutTopLevel(string scsPath)
+        {
+            var scsName = Path.GetFileName(scsPath);
+            var outputDir = Path.Combine(destination, scsName);
+            Directory.CreateDirectory(outputDir);
+
+            var reader = HashFsReader.Open(scsPath);
+
+            var associatedToDirectories = new HashSet<string>();
+            foreach (var (key, entry) in reader.GetEntries())
+            {
+                if (entry.IsDirectory)
+                {
+                    // locale.scs contains subdirectory listings,
+                    // but they're useless because the file names are relative
+                    continue;
+                }
+                reader.ExtractToFile(entry, Path.Combine(outputDir, key.ToString("x")));
+            }
+        }
+
+        private static void ExtractDirectory(HashFsReader r, string directory, string scsName)
         {
             Console.Out.WriteLine($"Extracting {scsName}{directory} ...");
 
@@ -99,7 +141,7 @@ namespace Extractor
             ExtractFiles(r, files);
 
             foreach (var subdir in subdirs)
-                Extract(r, subdir, scsName);
+                ExtractDirectory(r, subdir, scsName);
         }
 
         private static void ExtractFiles(HashFsReader r, List<string> files)
--- README.md
+++ README.md
@@ -5,10 +5,16 @@ An alternative .scs extractor written in C#. Supports partial extraction as well
 extractor path [options]
 
 Options:
-  -a, --all                  Extracts every .scs file in the directory.
+  -a, --all                  Extracts every .scs archive in the directory.
   -d=VALUE                   The output directory.
                                Default: ./extracted/
   -p=VALUE                   Partial extraction, e.g. "-p=/map".
+  -r, --raw                  Directly dumps the contained files with their
+                               hashed filenames rather than traversing the
+                               archive's directory tree.This allows for the
+                               extraction of base_cfg.scs, core.scs and
+                               locale.scs, which do not include a top level
+                               directory listing.
   -?, -h, --help             Prints this message.
 ```
 
