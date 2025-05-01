1ec43ad4875861812808d48e74dad96b3f332d17
sk-zk <sk-zk@users.noreply.github.com>
2025-05-01 15:04:26 +0200

    Add tests for pma/pmc/pmd heuristic

---
+++ Extractor.Tests/FileTypeHelperTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extractor.Deep;
+
+namespace Extractor.Tests
+{
+    public class FileTypeHelperTest
+    {
+        [Fact]
+        public void InferPmd()
+        {
+            var bytes = File.ReadAllBytes("Data/FileTypeHelperTest/sample.pmd");
+            Assert.Equal(FileType.Pmd, FileTypeHelper.Infer(bytes));
+        }
+
+        [Fact]
+        public void InferPmdNoParts()
+        {
+            var bytes = File.ReadAllBytes("Data/FileTypeHelperTest/sample_no_parts.pmd");
+            Assert.Equal(FileType.Pmd, FileTypeHelper.Infer(bytes));
+        }
+
+        [Fact]
+        public void InferPma()
+        {
+            var bytes = File.ReadAllBytes("Data/FileTypeHelperTest/sample.pma");
+            Assert.Equal(FileType.Pma, FileTypeHelper.Infer(bytes));
+        }
+
+        [Fact]
+        public void InferPmc()
+        {
+            var bytes = File.ReadAllBytes("Data/FileTypeHelperTest/sample.pmc");
+            Assert.Equal(FileType.Pmc, FileTypeHelper.Infer(bytes));
+        }
+    }
+}
