43bec8b22727a99b59efbd8143c1be7435321df0
sk-zk <sk-zk@users.noreply.github.com>
2025-07-16 23:39:04 +0200

    Expand ignore list and improve sound path parsing

--- Extractor/Deep/SiiPathFinder.cs
+++ Extractor/Deep/SiiPathFinder.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using TruckLib;
 using TruckLib.Sii;
 using static Extractor.PathUtils;
@@ -16,24 +15,39 @@ namespace Extractor.Deep
     internal class SiiPathFinder
     {
         private static readonly HashSet<string> ignorableUnitKeys = [
-            "vehicle_brands", "package_version", "compatible_versions", "suitable_for", "conflict_with",
+            "package_version", "compatible_versions", "suitable_for",
+            "conflict_with", "truck_y_override",
 
             // names
-            "category", "sign_name", "display_name", "ferry_name",
-            "static_lod_name", "city_name", "city_names", "city_name_localized", "board_name",
-            "sign_template_names",
+            "name", "names", "category", "sign_name", "display_name", "ferry_name",
+            "static_lod_name", "board_name", "sign_template_names", "editor_name",
+            "default_name", "stamp_name",
 
-            // curve_model stuff
-            "variation", "vegetation", "overlay",
+            // city
+            "city_name", "city_names", "city_name_localized", "short_city_name", 
+            "truck_lp_template",
 
-            "color_variant", // model stuff 
-            "allowed_vehicle", // traffic logic stuff
-            "overlay_schemes", // road stuff
-            "truck_lp_template", // city stuff
+            // curve_model
+            "variation", "vegetation", "overlay", "no_stretch_start", "no_stretch_end",
+
+            // vehicle
+            "vehicle_brands", "acc_list", "trailer_chains", "info",
+
+            // academy
+            "map_title", "map_desc", "intro_cutscene_tag", "start_position_tag",
+
+            // cutscene
+            "condition_data", "str_params",
+
+            "color_variant", // model 
+            "allowed_vehicle", // traffic logic
+            "overlay_schemes", // road
         ];
 
         private static readonly HashSet<string> ignorableUnitClasses = [
-            "cargo_def", "traffic_spawn_condition", "traffic_rule_data",
+            "cargo_def", "traffic_spawn_condition", "traffic_rule_data", "mail_text", "driver_names",
+            "localization_db", "input_device_config", "default_input_config", "default_input_entry",
+            "default_setup_entry", "academy_scenario_goal",
         ];
 
         public static HashSet<string> FindPathsInSii(byte[] fileBuffer, string filePath, IFileSystem fs)
@@ -110,9 +124,9 @@ namespace Extractor.Deep
             IList<dynamic> items = attrib.Value;
             var strings = items.Where(x => x is string).Cast<string>();
 
-            if (unitClass == "license_plate_data")
+            if (unitClass == "license_plate_data" || unitClass == "city_data")
             {
-                if (attrib.Key.StartsWith("def"))
+                if (attrib.Key.Contains("def"))
                 {
                     foreach (var str in strings)
                     {
@@ -130,20 +144,9 @@ namespace Extractor.Deep
             {
                 foreach (var str in strings)
                 {
-                    if (attrib.Key == "sounds")
+                    if (attrib.Key == "sounds" || attrib.Key == "sound_path")
                     {
-                        var pipeIdx = str.IndexOf('|');
-                        if (pipeIdx == -1)
-                            pipeIdx = 0;
-                        else
-                            pipeIdx += 1;
-
-                        var hashIdx = str.IndexOf('#');
-                        if (hashIdx == -1)
-                            hashIdx = str.Length;
-
-                        var soundPath = str[pipeIdx..hashIdx];
-                        PathFinder.Add(soundPath, potentialPaths, []);
+                        PathFinder.Add(GetSoundPath(str), potentialPaths, []);
                     }
                     else if (attrib.Key == "adr_info_icon" || attrib.Key == "fallback")
                     {
@@ -169,12 +172,32 @@ namespace Extractor.Deep
             }
         }
 
+        private static string GetSoundPath(string str)
+        {
+            var pipeIdx = str.IndexOf('|');
+            if (pipeIdx == -1)
+                pipeIdx = 0;
+            else
+                pipeIdx += 1;
+
+            var hashIdx = str.IndexOf('#');
+            if (hashIdx == -1)
+                hashIdx = str.Length;
+
+            var soundPath = str[pipeIdx..hashIdx];
+            return soundPath;
+        }
+
         private static void ProcessStringAttribute(KeyValuePair<string, dynamic> attrib, 
             string unitClass, HashSet<string> potentialPaths)
         {
+            string str = attrib.Value;
+
             if (unitClass == "ui::text"
                 || unitClass == "ui::text_template"
-                || unitClass == "ui_text_bar")
+                || unitClass == "ui_text_bar"
+                || (unitClass == "sign_template_text" && attrib.Key == "text")
+                || attrib.Key == "offence_message")
             {
                 // Extract paths from img and font tags of the faux-HTML
                 // used in UI strings.
@@ -195,10 +218,13 @@ namespace Extractor.Deep
                 }
                 PathFinder.Add(iconPath, potentialPaths, []);
             }
+            else if (attrib.Key == "sound_path" || attrib.Key == "sound_sfx" || attrib.Key == "scene_music")
+            {
+                PathFinder.Add(GetSoundPath(str), potentialPaths, []);
+            }
             else
             {
-                string s = attrib.Value;
-                PathFinder.Add(s, potentialPaths, []);
+                PathFinder.Add(str, potentialPaths, []);
             }
         }
     }
